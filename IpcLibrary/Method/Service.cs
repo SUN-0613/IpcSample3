0402fb6427a044c13c2ef5bc3801d89183f1d39c
AkihiroYamamoto <akiyam0613@gmail.com>
2019-10-24 11:19:15 +0900

    プロジェクト作成

---
+++ IpcLibrary/Method/Service.cs
@@ -0,0 +1,29 @@
+﻿using IpcLibrary.Properties;
+
+namespace IpcLibrary.Method
+{
+
+    public static class Service
+    {
+
+        /// <summary>アドレス取得</summary>
+        public static string GetAddress()
+        {
+            return Resource.Address + "/" + Resource.EndPoint;
+        }
+
+        /// <summary>ベースアドレス取得</summary>
+        public static string GetBaseAddress()
+        {
+            return Resource.Address;
+        }
+
+        /// <summary>エンドポイント取得</summary>
+        public static string GetEndpoint()
+        {
+            return Resource.EndPoint;
+        }
+
+    }
+
+}
